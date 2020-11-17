7f63e68c6928870c682c9e271cab2118661aedb6
Oscar Roberto Miranda Espinoza <rob.mir.92@outlook.es>
2020-11-17 15:41:37 -0800

    Add project files.

---
+++ Facade/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
